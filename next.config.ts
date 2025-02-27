510d934b14c89ddd28870cb3ad91d74b91cb6411
hari1050 <hariajax2000@gmail.com>
2025-02-27 18:26:40 +0530

    embedtest

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,22 @@ const nextConfig: NextConfig = {
       'lrtcczklgngprpbdspkn.supabase.co', // Supabase storage domain
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/widget-loader.js',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: '*' }
+        ],
+      },
+      {
+        source: '/widget/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: '*' }
+        ],
+      }
+    ]
+  },
 };
 
 export default nextConfig;
